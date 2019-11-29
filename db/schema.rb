e6e51f474cdcbd9602c6740cf35c70cad1ab1e53
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-11-29 20:45:21 +0900

    has_secure_passwordを使えるように

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_113326) do
+ActiveRecord::Schema.define(version: 2019_11_29_114130) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest", null: false
     t.index ["name"], name: "index_users_on_name"
   end
 
