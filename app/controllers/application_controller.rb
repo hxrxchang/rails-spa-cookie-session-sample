693dff49e093b5cc6dbac0a88e4e4c6f956fcdda
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-11-30 01:01:15 +0900

    application_controllerでエラーハンドリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404_error
+  rescue_from StandardError, with: :render_500_error
+
+  def render_401_error
+    render json: { message: "access is forbidden" }, status: 401
+  end
+
+  def render_404_error
+    render json: { message: "data is missing" }, status: 404
+  end
+
+  def render_500_error()
+    render json: { message: "internal server error" }, status: 500
+  end
 end
