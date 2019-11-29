bc9619dae44e2b6b7c533218035199fb47ad36b6
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-11-30 02:30:13 +0900

    CORSを有効に

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,13 @@ module RailsSpaCookieSessionSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "http://localhost:4200"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
