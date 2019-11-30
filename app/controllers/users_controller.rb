2134842e16a0c2bb75fc0c091fa5790dee44899e
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-12-01 00:14:34 +0900

    [API] ユーザー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :check_is_signed_in, only: [:sign_in, :sign_out, :check_session_valid]
+  skip_before_action :check_is_signed_in, only: [:sign_in, :sign_out, :check_session_valid, :sign_up]
 
   def sign_in
     user = User.find_by(name: params[:name]).authenticate(params[:password])
@@ -10,6 +10,21 @@ class UsersController < ApplicationController
   def sign_out
     session.delete(:user_id)
     render json: { message: "sign out success" }
+  end
+
+  def sign_up
+    begin
+      user = User.create!(name: params[:name], password: params[:password])
+    rescue ActiveRecord::RecordNotUnique => e
+      return render json: { message: "this name is already registered" }, status: 400
+    rescue ActiveRecord::RecordInvalid => e
+      return render json: { message: "this value is invalid" }, status: 400
+    rescue => e
+      return render_500_error
+    end
+
+    session[:user_id] = user.id
+    render json: { message: "sign up success" }
   end
 
   def check_session_valid
