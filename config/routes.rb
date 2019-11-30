9085d43e23a81aac33af87ab529e9e0574bb575b
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-11-30 14:43:13 +0900

    controller名変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     post '/auth/sign-in', to: 'users#sign_in'
     post '/auth/sign-out', to: 'users#sign_out'
-    get '/home', to: 'home#index'
+    get '/hello', to: 'hello#index'
   end
 end
